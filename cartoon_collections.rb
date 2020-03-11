66c1352367b8a2b1deff4213b7a7ea4906312aa6
Bikram Sodhi <biksodhi@gmail.com>
2020-03-11 23:11:01 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, i|
+    puts "#{i+1} #{name}"
+  end
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planet)
+  planet.map do |name|
+    "#{name.capitalize}!"
+  end
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
+  calls.any? do |size|
+    size.length > 4
+  end
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  list.find do |maybe_cheese|
+    cheese_types.include?(maybe_cheese)
+  end
+  # the array below is here to help
+  
 end
